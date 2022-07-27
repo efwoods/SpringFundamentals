b59f80fb2d07515ebec222b84bd4e74c5533aaff
efwoods <22337004+efwoods@users.noreply.github.com>
2022-07-27 10:01:51 -0400

    commit

--- src/main/java/com/example/demo/SpringFundamentalsApplication.java
+++ src/main/java/com/example/demo/SpringFundamentalsApplication.java
@@ -2,12 +2,26 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.text.NumberFormat;
+import java.util.Locale;
 
 @SpringBootApplication
 public class SpringFundamentalsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringFundamentalsApplication.class, args);
+	}
+
+	@Bean
+	public NumberFormat defaultCurrencyFormat() {
+		return NumberFormat.getCurrencyInstance();
+	}
+
+	@Bean
+	public NumberFormat germanCurrencyFormat() {
+		return NumberFormat.getCurrencyInstance(Locale.GERMANY);
 	}
 
 }
--- src/test/java/com/example/demo/SpringFundamentalsApplicationTests.java
+++ src/test/java/com/example/demo/SpringFundamentalsApplicationTests.java
@@ -1,13 +1,44 @@
 package com.example.demo;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import java.text.NumberFormat;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class SpringFundamentalsApplicationTests {
+	@Autowired
+	private ApplicationContext ctx;
+
+	@Autowired @Qualifier("defaultCurrencyFormat")
+	private NumberFormat nf;
+
+	@Test
+	public void defaultCurrency() {
+		double amount = 1234.9823749;
+		System.out.println(nf.format(amount));
+	}
+
+	@Test
+	public void germanCurrency() {
+		NumberFormat deutschNF = ctx.getBean("germanCurrencyFormat", NumberFormat.class);
+		double amount = 1234.9823749;
+		System.out.println(deutschNF.format(amount));
+	}
 
 	@Test
 	void contextLoads() {
+		int count = ctx.getBeanDefinitionCount();
+		System.out.println("There are " + count + " beans in the application context");
+		for (String name : ctx.getBeanDefinitionNames()){
+			System.out.println(name);
+		}
 	}
 
 }
